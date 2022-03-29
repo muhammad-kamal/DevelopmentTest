437bdd9015b75c4be95c7e0a2625b800b942a1d7
muhammad-kamal <Muhammad.Kamal.RA@gmail.com>
2022-03-29 05:00:21 +0300

    update

--- myLaravel/app/Models/User.php
+++ myLaravel/app/Models/User.php
@@ -8,8 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
 
-    protected $fillable =['name'];
+    protected $fillable =['name','password','email','remember_token'];
 
+    protected $attributes = array(
+        'surname' => "don't have a surname" ,
+        'password' => "0",
+        'remember_token' => '0'
+      );
 
     protected $primaryKey = "userid";
     use HasFactory;
